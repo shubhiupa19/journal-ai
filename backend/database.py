36b12e437cabed23e70d32634190126bb33b8e48
shubhiupa19 <s.x.upadhyay@gmail.com>
2026-02-12 12:35:47 -0500

    improved pipeline for incorporating feedback into model

--- backend/database.py
+++ backend/database.py
@@ -1,5 +1,6 @@
 import sqlite3
 from datetime import datetime
+
 DATABASE_PATH = "feedback.db"
 
 
@@ -11,7 +12,8 @@ def init_db():
     cursor = conn.cursor()
 
     # create feedback table if it doesn't exist
-    cursor.execute(""" CREATE TABLE IF NOT EXISTS feedback (
+    cursor.execute(
+        """ CREATE TABLE IF NOT EXISTS feedback (
                         id INTEGER PRIMARY KEY AUTOINCREMENT,
                    text TEXT NOT NULL,
                    predicted_distortion TEXT, 
@@ -21,29 +23,35 @@ def init_db():
                    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
                    used_in_training BOOLEAN DEFAULT FALSE)
                    """
-                   )
+    )
     # create model_versions table if it doesn't exist
-    cursor.execute( """ CREATE TABLE IF NOT EXISTS model_versions (
+    cursor.execute(
+        """ CREATE TABLE IF NOT EXISTS model_versions (
                    id INTEGER PRIMARY KEY AUTOINCREMENT,
                    version_number INTEGER,
                    training_samples INTEGER,
                    accuracy REAL,
                    notes TEXT
-                   )""")
-    
+                   )"""
+    )
+
     # save the changes via
     conn.commit()
 
     # close the connection
     conn.close()
 
+
 def save_feedback(text, predicted_distortion, user_correction, is_accepted, confidence):
     conn = sqlite3.connect(DATABASE_PATH)
     cursor = conn.cursor()
 
     # add data point to feedback table
-    cursor.execute(""" INSERT INTO feedback (text, predicted_distortion, user_correction, is_accepted, confidence)
-                   VALUES (?,?,?,?,?)""", (text, predicted_distortion, user_correction, is_accepted, confidence))
+    cursor.execute(
+        """ INSERT INTO feedback (text, predicted_distortion, user_correction, is_accepted, confidence)
+                   VALUES (?,?,?,?,?)""",
+        (text, predicted_distortion, user_correction, is_accepted, confidence),
+    )
 
     # save changes
     conn.commit()
@@ -54,6 +62,7 @@ def save_feedback(text, predicted_distortion, user_correction, is_accepted, conf
     conn.close()
 
     return feedback_id
+
 
 def retrieve_feedback():
     conn = sqlite3.connect(DATABASE_PATH)
@@ -68,6 +77,62 @@ def retrieve_feedback():
     return table
 
 
+def get_training_feedback():
+    conn = sqlite3.connect(DATABASE_PATH)
+    cursor = conn.cursor()
+    cursor.execute(
+        """ SELECT text, user_correction FROM feedback WHERE is_accepted IS FALSE AND used_in_training IS FALSE"""
+    )
+
+    table = cursor.fetchall()
+
+    conn.close()
+
+    return table
+
+
+def mark_used_feedback():
+    conn = sqlite3.connect(DATABASE_PATH)
+    cursor = conn.cursor()
+    cursor.execute(
+        """ UPDATE feedback SET used_in_training=TRUE WHERE is_accepted=FALSE and used_in_training=FALSE"""
+    )
+    conn.commit()
+    conn.close()
+
+
+def save_model_version(version_number, training_samples, accuracy, notes):
+    conn = sqlite3.connect(DATABASE_PATH)
+    cursor = conn.cursor()
+
+    # add new version to model_versions table
+    cursor.execute(
+        """ INSERT INTO model_versions (version_number, training_samples, accuracy, notes)
+                   VALUES (?,?,?,?)""",
+        (version_number, training_samples, accuracy, notes),
+    )
+
+    # save changes
+    conn.commit()
+
+    # get the most recent row of model_versions
+    model_version_id = cursor.lastrowid
+
+    conn.close()
+
+    return model_version_id
+
+
+def get_latest_version():
+    conn = sqlite3.connect(DATABASE_PATH)
+    cursor = conn.cursor()
+    cursor.execute(""" SELECT MAX(version_number) FROM model_versions """)
+    version_number = cursor.fetchone()
+    conn.close()
+    if version_number[0] is None:
+        return 1
+    else:
+        return version_number[0]
 
 if __name__ == "__main__":
     print("Initializing database...")
--- backend/train_model.py
+++ backend/train_model.py
@@ -6,16 +6,34 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report, accuracy_score, confusion_matrix
 import joblib
 import numpy as np
+from database import (
+    get_training_feedback,
+    mark_used_feedback,
+    save_model_version,
+    get_latest_version,
+)
 
-# Load dataset
-df = pd.read_csv('cognitive_distortion_dataset.csv')
+# Load Kaggle dataset
+kaggle_df = pd.read_csv("cognitive_distortion_dataset.csv")
 
 # Drop empty rows and any without a dominant distortion label
-df = df.dropna(subset=['Patient Question', 'Dominant Distortion'])
+kaggle_df = kaggle_df.dropna(subset=["Patient Question", "Dominant Distortion"])
+
+# Store this in the primary df
+df = kaggle_df
+
+# Load user feedback
+feedback_data = get_training_feedback()
+if len(feedback_data) > 0:
+    feedback_df = pd.DataFrame(
+        feedback_data, columns=["Patient Question", "Dominant Distortion"]
+    )
+    df = pd.concat([kaggle_df, feedback_df])
+
 
 # Prepare features and labels
-X = df['Patient Question']
-y = df['Dominant Distortion']
+X = df["Patient Question"]
+y = df["Dominant Distortion"]
 
 # Train/test split
 X_train, X_test, y_train, y_test = train_test_split(
@@ -23,29 +41,40 @@ X_train, X_test, y_train, y_test = train_test_split(
 )
 
 # Build pipeline for ML: TF-IDF + Logistic Regression Model
-pipeline = Pipeline([
-    # first step in the pipeline is creating feature vectors using TF-IDF
-    ('tfidf', TfidfVectorizer(
-        stop_words='english',
-        ngram_range=(1, 2),  # only use unigrams and bigrams, which are often more informative for text classification tasks than all possible combinations of words
-        max_features=5000,   # reduce the vocabulary size, which can increase accuracy because it reduces noise from less informative words
-        min_df=2,            # skip terms that appear in less than 2 documents, since they are too rare to be useful
-        max_df=0.8           # skip terms that appear in more than 80% of documents, since they aren't indicative of specific distortion types
-    )),
-    # second step in the pipeline is feeding the vectors to a Logistic Regression classifier
-    # so that we can get probability estimates on the test data 
-    # we are also using class_weight='balanced' to handle any class imbalance, which occurs when classes (types of distortions) are not equally represented in the dataset
-    # this imbalance is problematic because the model may become biased towards the majority class (No Distortion in this case) and perform poorly on minority classes
-    # we are also setting max_iter to 1000 to ensure convergence, which means the model has enough iterations to find the optimal solution
-    # we are also using regularization, which reduces weights on specific words that aren't indicative of a particular distortion type
-    # lastly, we are using the lbfgs sovler to find the optimal weights for the LR model
-    ('clf', LogisticRegression(
-        max_iter=1000,
-        class_weight='balanced',
-        C=1.0,               # Regularization strength
-        solver='lbfgs'       # Better solver for multiclass
-    )),
-])
+pipeline = Pipeline(
+    [
+        # first step in the pipeline is creating feature vectors using TF-IDF
+        (
+            "tfidf",
+            TfidfVectorizer(
+                stop_words="english",
+                ngram_range=(
+                    1,
+                    2,
+                ),  # only use unigrams and bigrams, which are often more informative for text classification tasks than all possible combinations of words
+                max_features=5000,  # reduce the vocabulary size, which can increase accuracy because it reduces noise from less informative words
+                min_df=2,  # skip terms that appear in less than 2 documents, since they are too rare to be useful
+                max_df=0.8,  # skip terms that appear in more than 80% of documents, since they aren't indicative of specific distortion types
+            ),
+        ),
+        # second step in the pipeline is feeding the vectors to a Logistic Regression classifier
+        # so that we can get probability estimates on the test data
+        # we are also using class_weight='balanced' to handle any class imbalance, which occurs when classes (types of distortions) are not equally represented in the dataset
+        # this imbalance is problematic because the model may become biased towards the majority class (No Distortion in this case) and perform poorly on minority classes
+        # we are also setting max_iter to 1000 to ensure convergence, which means the model has enough iterations to find the optimal solution
+        # we are also using regularization, which reduces weights on specific words that aren't indicative of a particular distortion type
+        # lastly, we are using the lbfgs sovler to find the optimal weights for the LR model
+        (
+            "clf",
+            LogisticRegression(
+                max_iter=1000,
+                class_weight="balanced",
+                C=1.0,  # Regularization strength
+                solver="lbfgs",  # Better solver for multiclass
+            ),
+        ),
+    ]
+)
 
 # fit the model to the training data
 pipeline.fit(X_train, y_train)
@@ -64,7 +93,7 @@ print(f"Test set size: {len(y_test)}")
 print(f"\nClass distribution in test set:")
 print(y_test.value_counts().sort_index())
 
-# using the classification report method from sklearn, print out a detailed classification report, which 
+# using the classification report method from sklearn, print out a detailed classification report, which
 # contains info such as precision, recall, and F1-score for each class
 print("Classification Report:")
 
@@ -72,5 +101,23 @@ print(classification_report(y_test, y_pred))
 
 # save the model to a file and essentially cache it for later use in the Flask API
 # this reduces latency for the end user since we don't have to retrain the model on every request / text that the user enters
-joblib.dump(pipeline, 'distortion_model.pkl')
+joblib.dump(pipeline, "distortion_model.pkl")
 
+try:
+    if len(feedback_data) > 0:
+        mark_used_feedback()
+except Exception as e:
+    print(f"Error with marking feedback as used in the database: {e}")
+
+try:
+    new_version = get_latest_version() + 1
+    save_model_version(
+        new_version,
+        len(X_train),
+        accuracy,
+        f"Trained with {len(feedback_data)} new feedback samples",
+    )
+except Exception as e:
+    print(f"Error with saving model: {e}")
+
+print("Model successfully saved!")
